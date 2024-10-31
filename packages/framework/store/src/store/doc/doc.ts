4b4bb7adda99a203741029e2c8abbbee18cdc287
zzj3720 <zuozijian1994@gmail.com>
2024-10-31 12:06:57 +0800

    fix(database): use copy logic when creating a linked doc

--- packages/framework/store/src/store/doc/doc.ts
+++ packages/framework/store/src/store/doc/doc.ts
@@ -3,6 +3,7 @@ import { type Disposable, Slot } from '@blocksuite/global/utils';
 import { signal } from '@preact/signals-core';
 
 import type { BlockModel, Schema } from '../../schema/index.js';
+import type { DraftModel } from '../../transformer/index.js';
 import type { BlockOptions } from './block/index.js';
 import type { BlockCollection, BlockProps } from './block-collection.js';
 import type { DocCRUD } from './crud.js';
@@ -507,9 +508,9 @@ export class Doc {
   }
 
   deleteBlock(
-    model: BlockModel,
+    model: DraftModel,
     options: {
-      bringChildrenTo?: BlockModel;
+      bringChildrenTo?: DraftModel;
       deleteChildren?: boolean;
     } = {
       deleteChildren: true,
